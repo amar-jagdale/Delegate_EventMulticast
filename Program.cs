21a12b1cdbf731d52c520c03f9f0355f5f1a4943
hp <hp@DESKTOP-TL0FRLL>
2021-08-28 16:50:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegate_EventMulticast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Delegate_Multicast");
+            MulticlassExample obj = new MulticlassExample();
+            MulticlassExample.mydelegate mydelegateInstance = null;
+
+            mydelegateInstance += new MulticlassExample.mydelegate(obj.Add);
+            mydelegateInstance += new MulticlassExample.mydelegate(obj.Mul);
+
+
+            mydelegateInstance(15,15);
+            mydelegateInstance(10,10);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+    class MulticlassExample
+    {
+        public delegate void mydelegate(int a, int b);
+        public void Add(int i, int j)
+        {
+            Console.WriteLine(i+j);
+
+        }
+
+        public void Mul(int i, int j)
+        {
+            Console.WriteLine(i * j);
+        }
+    }
+
+
+}
